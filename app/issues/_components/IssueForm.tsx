6d531efc52a1cce252239b234d9c0c18a0700cb6
ArfinHasib <arfinhasib21375@gmail.com>
2024-09-01 20:56:46 +0600

    Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -14,9 +14,7 @@ import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 import { Issue } from '@prisma/client';
 import { issueSchema } from '@/app/validationSchema';
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-    ssr: false,
-});
+import SimpleMDE from 'react-simplemde-editor'
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
