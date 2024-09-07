5047cfb0f1a6d3afafe8f4107897fe4c33434297
ArfinHasib <arfinhasib21375@gmail.com>
2024-09-07 16:27:26 +0600

    Fix filtering bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,11 +2,12 @@
 
 import { Status } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const IssueStatusFilter = () => {
     const router = useRouter()
+    const searchParams = useSearchParams()
 
     const statuses: { label: string, value?: Status }[] = [
         { label: "All" },
@@ -17,19 +18,25 @@ const IssueStatusFilter = () => {
 
 
 
-  return (
-    <Select.Root onValueChange={(status) => {
-        const query= status ? `?status=${status}` : ""
-        router.push("/issues/list" + query)
+    return (
+        <Select.Root defaultValue={searchParams.get("status") || ""} onValueChange={(status) => {
+            const params = new URLSearchParams()
+            if (status) params.append("status", status)
+            if (searchParams.get("orderBy"))
+                params.append("orderBy", searchParams.get("orderBy")!)
+
+            searchParams.get("orderBy")
+            const query = params.size ? "?" + params.toString() : ""
+            router.push("/issues/list" + query)
         }}>
-        <Select.Trigger placeholder='Filter by status...'/>
-        <Select.Content>
-            {statuses.map((status) => (
-                <Select.Item key={status.value} value={status.value || "unassign"}>{status.label}</Select.Item>
-            ))}
-        </Select.Content>
-    </Select.Root>
-  )
+            <Select.Trigger placeholder='Filter by status...' />
+            <Select.Content>
+                {statuses.map((status) => (
+                    <Select.Item key={status.value} value={status.value || "unassign"}>{status.label}</Select.Item>
+                ))}
+            </Select.Content>
+        </Select.Root>
+    )
 }
 
 export default IssueStatusFilter
