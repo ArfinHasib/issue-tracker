e6efd61b218f3da6fe03e2c19b47e692acd777d9
ArfinHasib <arfinhasib21375@gmail.com>
2024-09-07 16:08:04 +0600

    Sort Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -7,78 +7,81 @@ import { Issue, Status } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams: { status: Status, orderBy: keyof Issue };
+    searchParams: { status: Status, orderBy: keyof Issue };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: {
-    label: string;
-    value: keyof Issue;
-    className?: string;
-  }[] = [
-    { label: "Issue", value: "title" },
-    {
-      label: "Status",
-      value: "status",
-      className: "hidden md:table-cell",
-    },
-    {
-      label: "Created",
-      value: "createdAt",
-      className: "hidden md:table-cell",
-    },
-  ];
+    const columns: {
+        label: string;
+        value: keyof Issue;
+        className?: string;
+    }[] = [
+            { label: "Issue", value: "title" },
+            {
+                label: "Status",
+                value: "status",
+                className: "hidden md:table-cell",
+            },
+            {
+                label: "Created",
+                value: "createdAt",
+                className: "hidden md:table-cell",
+            },
+        ];
 
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
+    const statuses = Object.values(Status);
+    const status = statuses.includes(searchParams.status)
+        ? searchParams.status
+        : undefined;
 
-  const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
-  });
+    const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) ? { [searchParams.orderBy]: "asc" } : undefined
 
-  return (
-    <div>
-      <IssueActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
-                <NextLink href={{
-                  query: { ...searchParams, orderBy: column.value }
-                }}>{column.label}</NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </div>
-  );
+    const issues = await prisma.issue.findMany({
+        where: {
+            status,
+        },
+        orderBy
+    });
+
+    return (
+        <div>
+            <IssueActions />
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        {columns.map((column) => (
+                            <Table.ColumnHeaderCell key={column.value}>
+                                <NextLink href={{
+                                    query: { ...searchParams, orderBy: column.value }
+                                }}>{column.label}</NextLink>
+                                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
+                            </Table.ColumnHeaderCell>
+                        ))}
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue) => (
+                        <Table.Row key={issue.id}>
+                            <Table.Cell>
+                                <Link href={`/issues/${issue.id}`}>
+                                    {issue.title}
+                                </Link>
+                                <div className="block md:hidden">
+                                    <IssueStatusBadge status={issue.status} />
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                <IssueStatusBadge status={issue.status} />
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                {issue.createdAt.toDateString()}
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
+        </div>
+    );
 };
 
 export const dynamic = "force-dynamic";
