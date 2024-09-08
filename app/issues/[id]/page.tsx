8a3a3d2928d3eecc27c5f4b1b01df16c9cbb1ecd
ArfinHasib <arfinhasib21375@gmail.com>
2024-09-08 18:05:40 +0600

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -10,35 +10,48 @@ import authOptions from "@/app/auth/AuthOptions";
 import AssigneeSelect from "./AssigneeSelect";
 
 interface Props {
-    params: { id: string };
+	params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-    // if (typeof params.id !== "number") notFound();
-    const session = await getServerSession(authOptions)
+	// if (typeof params.id !== "number") notFound();
+	const session = await getServerSession(authOptions);
 
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id) },
-    });
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
 
-    if (!issue) notFound();
+	if (!issue) notFound();
 
-    await delay(2000);
+	await delay(2000);
 
-    return (
-        <Grid columns={{ initial: "1", sm: "5" }} gap={"5"}>
-            <Box className="md:col-span-4">
-                <IssueDetails issue={issue} />
-            </Box>
-            {session && <Box>
-                <Flex direction="column" gap={"4"}>
-                    <AssigneeSelect issue={issue} />
-                    <EditIssueButton issueId={issue.id} />
-                    <DeleteIssueButton issueId={issue.id} />
-                </Flex>
-            </Box>}
-        </Grid>
-    );
+	return (
+		<Grid columns={{ initial: "1", sm: "5" }} gap={"5"}>
+			<Box className='md:col-span-4'>
+				<IssueDetails issue={issue} />
+			</Box>
+			{session && (
+				<Box>
+					<Flex direction='column' gap={"4"}>
+						<AssigneeSelect issue={issue} />
+						<EditIssueButton issueId={issue.id} />
+						<DeleteIssueButton issueId={issue.id} />
+					</Flex>
+				</Box>
+			)}
+		</Grid>
+	);
 };
+
+export async function generateMetadata({ params }: Props) {
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+
+	return {
+		title: issue?.title,
+		description: "Details of issue" + issue?.id,
+	};
+}
 
 export default IssueDetailPage;
